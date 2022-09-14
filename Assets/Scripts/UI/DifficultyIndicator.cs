fe1e4432874b6b12157d17559520628dd51c8555
postive12 <48992839+postive12@users.noreply.github.com>
2022-09-14 21:51:31 +0900

    최종 결과물

--- Assets/Scripts/UI/DifficultyIndicator.cs
+++ Assets/Scripts/UI/DifficultyIndicator.cs
@@ -10,17 +10,17 @@ namespace UI
         [SerializeField] private TextMeshProUGUI _difficultyText;
         public void UpdateDifficulty()
         {
-            var targetText = "쉬움";
+            var targetText = "보통";
             switch (GameManager.Current.CurrentPlayerDifficulty)
             {
                 case 1:
-                    targetText = "쉬움";
-                    break;
-                case 2:
                     targetText = "보통";
                     break;
-                case 3:
+                case 2:
                     targetText = "어려움";
+                    break;
+                case 3:
+                    targetText = "아주 어려움";
                     break;
             }
             _difficultyText.text = "현재 난이도\n<color=red>" + targetText;
